884b6fae98ae728c95e9976f3b375356747eb047
bipinparajuli <bipinprjl@gmail.com>
2022-01-25 09:09:10 +0545

    analystics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,28 @@
 import '../styles/global.css'
-export default function App({Component,pageProps}){
-    return <Component {...pageProps} />
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
 }
+
+export default MyApp
